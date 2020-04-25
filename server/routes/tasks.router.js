a92a7cf86667b60fb14d96d3fec9a57e55b21e17
Alan Henderson <mr.alan.henderson@gmail.com>
2020-04-25 11:14:32 -0500

    server spin up successful

---
+++ server/routes/tasks.router.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+let pg = require('pg');
+
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'weekend-to-do-list',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', ()=> {
+    console.log('postgres connected');
+});
+pool.on('error', (error) => {
+    console.log('eror with postgres pool', error); 
+});
+
+//GET Route
+
+//POST Route
+
+//Delete Route
+
+//Put Route
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require( 'body-parser' );
+
+app.use( express.static( 'sever/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+const tasksRouter = require('./routes/tasks.router.js');
+app.use('/tasks', tasksRouter );
+
+app.listen(5000, ()=>{
+    console.log( 'server listening at http://localhost:5000');
+})
+
