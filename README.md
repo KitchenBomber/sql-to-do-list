df48a05d65a490ecabd97fb3096e88ff9e9ec7f7
Alan Henderson <mr.alan.henderson@gmail.com>
2020-04-26 11:27:39 -0500

    almost forgot to update the sql with an altered table I ended up using

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This app will allow multiple users to view a shared to-do list. Ideal for managi
 Usera will be able to add items to the list and check them off as well as remove items entirely when they are no longer needed
 ![Image 2](server/public/images/TODO2.png)
 
-Paid Version coming
+Paid Version with enhanced features coming soon!
--- databas.sql
+++ databas.sql
@@ -1,22 +1,21 @@
 CREATE TABLE tasks
 (
     "id" SERIAL PRIMARY KEY,
-    "task_description" VARCHAR (100),
-    "task_completed" BOOLEAN DEFAULT FALSE,
-    "date_added" DATE DEFAULT NOW(),
-    "date_due" DATE,
-    "date_completed" DATE,
-    "task_category" VARCHAR (25) Default 'General'
+    "description" VARCHAR (100),
+    "completed" BOOLEAN DEFAULT FALSE,
+    "added" DATE DEFAULT NOW(),
+    "due" DATE,
+    "done" DATE,
+    "category" VARCHAR (25) Default 'General'
 );
 
 
-
 INSERT INTO "tasks"
-    ("task_description", "date_added", "task_category")
+    ("description", "added", "category")
 VALUES
     ('build TODO app, basemode', NOW(), 'Prime Homework');
 
 INSERT INTO "tasks"
-    ("task_description", "date_added", "task_category")
+    ("description", "added", "category")
 VALUES
     ('build TODO app, stretchmode', NOW(), 'Prime Homework');
